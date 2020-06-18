e5171ac01a9c3d16714ac9c9165404d46b1572d8
heleneiscool <46540694+heleneiscool@users.noreply.github.com>
2020-06-18 22:27:38 +1200

    Update: core/completion
    works with the updated imageRender.hpp and speedDecider.hpp files to set the appropriate speed for the motor while the program is running
    tested for core and completion

--- robot.cpp
+++ robot.cpp
@@ -1,12 +1,18 @@
 #include "imageRender.hpp"
 #include "speedDecider.hpp"
+
+
 int main(){
+
 	if (initClientRobot() !=0){
 		std::cout<<" Error initializing robot"<<std::endl;
-	}
+	}   
     while(1){
-		setMotorCore(renderImageCore());  
-        usleep(10000);
-  } //while
+		takePicture();
+		// While true is going in a loop move the car
+		setMotorCompletion(renderImageCompletion());
 
+
+
+  } //while
 } // main
