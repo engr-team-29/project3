11fafcf371081e22aefed41359938ad13bfefce3
heleneiscool <46540694+heleneiscool@users.noreply.github.com>
2020-06-18 22:24:34 +1200

    Update: core/completion
    renders the image for core and completion. gives information about the presences and whereabouts of lines on the screen.
    works for both core and completion. a different function would be needed for challenge.
    returns a 'linereading' struct

--- imageRender.hpp
+++ imageRender.hpp
@@ -1,41 +1,44 @@
 #include "robot.hpp"
+/*****Line reading, values for lines from image*****/
+struct LineReading{
+	int leftLine=0;
+	int leftPosition=0;
+	int rightLine=0;
+	int rightPosition=0;
+	int topLine=0;
+	int topPosition=0;
+	int bottomLine=0;
+	int bottomPosition=0;
+};
 
-int* renderImageCore(){
-
-	takePicture();
-
-    SavePPMFile("i0.ppm",cameraView);
-
-    int imageSize = cameraView.height * cameraView.width;
-
-	int*  pixels = new int[imageSize]; 
-//scan horizontal array and then divide all by 2 and if pix > 200 then its white and then change motor speed. if n of array is >200 on right side then lower speed vRight
-
-	
-	OpenPPMFile("i0.ppm", cameraView);
-    // for pixel in image
-
-		//get the pixel:
-		int curPix; //current pixel get_pixel( ImagePPM image,int row,int col, int color)
-		for(int col = 0; col < cameraView.width; col++){
-			for(int row = 0; row < cameraView.height; row++){
-				curPix = get_pixel(cameraView, row, col, 3);
-				if(curPix > 200) {
-					pixels[row*col+col] = 1;
-				}else {
-					pixels[row*col+col] = 0;
-				}	
-				
+LineReading renderImageCompletion(){
+	LineReading reading;
+			for(int row=0; row<cameraView.height; row++){
+				for(int col=0; col<cameraView.width; col++){
+					int brightness = get_pixel(cameraView, row, col, 3);
+					bool isWhite = brightness>=250 |brightness<2; //includes black so that the car hits the flag in the end
+					if(isWhite){
+					if(col == 0){                                
+						reading.leftPosition += row;
+						reading.leftLine++;
+					}
+					else if(col == cameraView.width-1){
+						
+				 reading.rightPosition += row;
+				 reading.rightLine++;
+					}
+					if(row == 0){
+						
+					reading.topPosition += col;
+				 reading.topLine++;
+						}
+						else if(row==cameraView.height-1){
+							
+							reading.bottomPosition += col;
+							reading.bottomLine++;
+						}
+				}
+				}
 			}
-		}
-		std::cout<<pixels<<endl;
-
-		//if white put 1 in the array at the index of that pixel
-
-		//otherwise put 0 in that index
-
-	//should return the list of 1s and 0s
-
-	return pixels;
-
+	return reading;
 }
